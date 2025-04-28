dae05c7bd988853cd7c1c95c377b0b3211d80e18
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-04-28 07:57:57 +0300

    fix route

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,13 +1,22 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAppSelector } from "@/app/hooks";
+import { useAppDispatch } from "@/app/hooks";
+import { setUser } from "@/features/auth/authSlice";
+import { STORAGE_KEYS } from "@/constants/storageKeys";
 
 const PrivateRoute: React.FC = () => {
+  const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.auth.user);
 
   if (!user) {
+    const email = localStorage.getItem(STORAGE_KEYS.email);
+    const uid = localStorage.getItem(STORAGE_KEYS.uid);
+    if (email && uid) {
+      dispatch(setUser({ email, uid }));
+      return null;
+    }
     return <Navigate to="/login" replace />;
   }
-
   return <Outlet />;
 };
 
